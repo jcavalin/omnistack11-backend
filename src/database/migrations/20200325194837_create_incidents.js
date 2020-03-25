7a95f9007572c37ec92bad2c1f46c3655eb9b74b
Julio Cavalin <julio.cavalin@gmail.com>
2020-03-25 20:04:26 -0300

    [CHORE] Migrations and structure

---
+++ src/database/migrations/20200325194837_create_incidents.js
@@ -0,0 +1,19 @@
+// npx knex migrate:make create_incidents
+
+exports.up = function(knex) {
+    return knex.schema.createTable('incidents', table => {
+        table.increments();
+        table.string('title').notNullable();
+        table.string('description').notNullable();
+        table.decimal('value').notNullable();
+        table.string('ong_id').notNullable();
+
+        table.foreign('ong_id').references('id').inTable('ongs');
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('incidents');
+};
+
+// npx knex migrate:latest
