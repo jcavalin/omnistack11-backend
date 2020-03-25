eeb0fed32a5e5639650f2fde5cfe189228ffe4ed
Julio Cavalin <julio.cavalin@gmail.com>
2020-03-25 20:47:58 -0300

    [FEAT] Incidents list by ong

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const OngController = require('./controllers/OngController');
-
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+
 
 const routes = express.Router();
 
@@ -12,5 +13,7 @@ routes.post('/ongs', OngController.create);
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
