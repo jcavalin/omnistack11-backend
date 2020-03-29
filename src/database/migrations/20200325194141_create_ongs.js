40cfcb66fc48743653a7f8424ab16be3ee8acfe6
Julio Cavalin <julio.cavalin@gmail.com>
2020-03-29 18:17:04 -0300

    [CHORE] Tests

---
+++ src/database/migrations/20200325194141_create_ongs.js
@@ -0,0 +1,19 @@
+
+// npx knex migrate:make create_ongs
+
+exports.up = function(knex) {
+    return knex.schema.createTable('ongs', table => {
+        table.string('id').primary();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('whatsapp').notNullable();
+        table.string('city').notNullable();
+        table.string('uf', 2).notNullable();
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('ongs');
+};
+
+// npx knex migrate:latest
